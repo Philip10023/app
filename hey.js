1ca2eac4aa1f5972a784a183835eb173db8b353c
Philip10023 <philipormorgan@gmail.com>
2018-10-19 15:18:33 -0400

    Initial Commit - probably ending here as well

---
+++ hey.js
@@ -0,0 +1,51 @@
+{
+  u'Name': 'listen-n-save-mp3',
+  'ResponseMetadata':
+  {
+    'HTTPStatusCode': 200,
+    'RetryAttempts': 0,
+    'HostId': 'U2Bf9YFl8Mrd1lq01AWNDDU7UEOCLpTI6ZOCM2E8y/+YV1n49O9KbAXJK352HwRpyrDNJ7BtraE=',
+    'RequestId': 'E6AA49D044C90D3D',
+    'HTTPHeaders':
+    {
+      'x-amz-bucket-region': 'us-east-1',
+      'x-amz-id-2': 'U2Bf9YFl8Mrd1lq01AWNDDU7UEOCLpTI6ZOCM2E8y/+YV1n49O9KbAXJK352HwRpyrDNJ7BtraE=',
+      'server': 'AmazonS3',
+      'transfer-encoding': 'chunked',
+      'x-amz-request-id': 'E6AA49D044C90D3D',
+      'date': 'Tue, 16 Oct 2018 19:24:58 GMT',
+      'content-type': 'application/xml'
+    }
+  },
+  u'Delimiter': u'/',
+  u'MaxKeys': 1000,
+  u'Prefix': 'philips%40exmaple.com/',
+  u'Marker': u'', u'EncodingType': 'url',
+  u'IsTruncated': False,
+  u'Contents':
+  [
+    {
+      u'LastModified': datetime.datetime(2018, 10, 16, 19, 22, 6, tzinfo=tzlocal()),
+      u'ETag': '"d41d8cd98f00b204e9800998ecf8427e"',
+      u'StorageClass': 'STANDARD',
+      u'Key': u'philips@exmaple.com/',
+      u'Owner':
+      {
+        u'DisplayName': 'listennsave',
+        u'ID': 'e4421bc564cce5393a0911888bf5fbacd8e97ef718b03a6e9e4934371b709e66'
+      },
+      u'Size': 0
+    }, {
+      u'LastModified': datetime.datetime(2018, 10, 16, 18, 57, 43, tzinfo=tzlocal()),
+        u'ETag': '"f71a18856b366be5f0718a9be494f137"',
+        u'StorageClass': 'STANDARD',
+        u'Key': u'philips@exmaple.com/supImanId.mp3',
+        u'Owner':
+        {
+          u'DisplayName': 'listennsave',
+          u'ID': 'e4421bc564cce5393a0911888bf5fbacd8e97ef718b03a6e9e4934371b709e66'
+        },
+        u'Size': 36942
+      }
+    ]
+  }
